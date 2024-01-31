9745276d28c97e73b2ce11b1647809d176bc6191
Sandy <sandeshchapagai@users.noreply.github.com>
2024-01-31 16:27:13 +0545

    multi level chaining

--- Acess_MOdifier/src/Main.java
+++ Acess_MOdifier/src/Main.java
@@ -1,7 +1,8 @@
 public  class Main {
 
     public static void main(String[] args) {
-        int final_result=new Calculator().add(3,5).sub(2,3).result;
+
+        int final_result=new Calculator().add(3,5).sub(9,3).GetResult();
         System.out.println(final_result);
     }
 }
