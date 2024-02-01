38e763acaa295476e02344fc4b9ba819f7013716
Sandy <sandeshchapagai@users.noreply.github.com>
2024-02-01 17:00:13 +0545

    expense_traces started and publish

---
+++ Expense_Trace/src/Main.java
@@ -0,0 +1,25 @@
+import jdk.jfr.Category;
+class Expense {
+    double amount;
+    DateTime date;
+    Category category;
+    String user;
+
+    void addExpenses(){
+
+     }
+
+     void editExpenses(){
+
+     }
+
+     void deleteExpenses(){
+
+     }
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+    }
+}
--- interfaces/src/Main.java
+++ interfaces/src/Main.java
@@ -1,18 +1,17 @@
-abstract class Game{
-public  abstract void  print();
-}
+import java.util.ArrayList;
 
-class  Football extends  Game {
-
-    @Override
-    public void print() {
-        System.out.println("k xaaa");
-    }
-}
-
-class  Main{
-    public static   void main(String[] args){
-        Football myGame = new Football();
-        myGame.print();
+class Main{
+    public  static  void main(String[] args) {
+        ArrayList<String> games = new ArrayList<>();
+        games.add("dajksf");
+        games.add("bsdakj");
+        games.add("jnvsdakj");
+        games.add("kjbakj");
+//        games.set(2, "whats up");
+//        games.set(3, "nono");
+//        games.set(0, "knakjf");
+        for (int i = 0; i < games.size(); i++) {
+            System.out.println(games.get(i));
+        }
     }
 }
