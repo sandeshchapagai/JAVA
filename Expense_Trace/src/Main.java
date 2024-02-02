10a81a69bc052a25b346cb18b877272a5874c597
Sandy <sandeshchapagai@users.noreply.github.com>
2024-02-02 13:13:48 +0545

    geeter setter and inheritences

--- Expense_Trace/src/Main.java
+++ Expense_Trace/src/Main.java
@@ -1,20 +1,34 @@
 
 class Number{
+    private int  number;
     public int add(int x,int y){
-        return x*y;
+        return x*y+number;
     }
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int newNumber){
+        this.number=newNumber;
+    }
+}
+class Hello extends Number{
+//    public int j=cap.add(4,5);
+protected   int  integerNumber = 7;
 }
 public class Main {
     public static int add(int x, int y){
           return  x+y;
     }
     public static void main(String[] a ) {
-        System.out.println("hello world");
-          int y=add(8,6);
-        System.out.println(y);
-        Number hello = new Number();
-        int z = hello.add(3,2);
-        System.out.println(z);
+
+        Hello cap = new Hello();
+        cap.setNumber(8);
+        int m = cap.add(7,6);
+        System.out.println(m+cap.integerNumber);
+
+
+
 
 
     }
