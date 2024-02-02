49774a7f32b95590dfe5291d82a55d8fc461d6d3
Sandy <sandeshchapagai@users.noreply.github.com>
2024-02-02 12:33:56 +0545

    known about static and public diffrence

--- Expense_Trace/src/Main.java
+++ Expense_Trace/src/Main.java
@@ -1,25 +1,22 @@
-import jdk.jfr.Category;
-class Expense {
-    double amount;
-    DateTime date;
-    Category category;
-    String user;
 
-    void addExpenses(){
-
-     }
-
-     void editExpenses(){
-
-     }
-
-     void deleteExpenses(){
-
-     }
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
+class Number{
+    public int add(int x,int y){
+        return x*y;
     }
 }
+public class Main {
+    public static int add(int x, int y){
+          return  x+y;
+    }
+    public static void main(String[] a ) {
+        System.out.println("hello world");
+          int y=add(8,6);
+        System.out.println(y);
+        Number hello = new Number();
+        int z = hello.add(3,2);
+        System.out.println(z);
+
+
+    }
+
+    }
